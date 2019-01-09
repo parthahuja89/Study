a3fd56c2ec8053f4d991349f0c5af158d6b2fa96
parthahuj@gmail.com <parthahuj@gmail.com>
2019-01-09 12:51:10 -0800

    init commit

---
+++ app/src/main/java/me/parthahuja/studymate/MainPage.kt
@@ -0,0 +1,36 @@
+package me.parthahuja.studymate
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.TextView
+
+import kotlinx.android.synthetic.main.activity_main_page.*
+import java.util.*
+
+class MainPage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_page)
+
+
+        val button_element = findViewById<Button>(R.id.roll)
+
+        //listening for button_element click
+
+        button_element.setOnClickListener {
+            routeSecond()
+        }
+    }
+
+    fun routeSecond(){
+        val route = Intent(this, second_activity::class.java)
+        startActivity(route)
+    }
+}
