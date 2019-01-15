2e4186fbba1ece9254ca88f1028ded18df2f40e3
parthahuj@gmail.com <parthahuj@gmail.com>
2019-01-14 19:41:33 -0800

    adding dialog

--- app/src/main/java/me/parthahuja/studymate/second_activity.kt
+++ app/src/main/java/me/parthahuja/studymate/second_activity.kt
@@ -3,39 +3,12 @@ package me.parthahuja.studymate
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button
+import android.widget.TextView
 
 import kotlinx.android.synthetic.main.activity_second_activity.*
 
 class second_activity : AppCompatActivity() {
-
-    /*
-     * Method Runs when onCreate View
-     */
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second_activity)
-        setSupportActionBar(toolbar)
-
-        //Add card Fab Id
-        var add_card = findViewById<FloatingActionButton>(R.id.add_button)
-
-        //Listener for Fab
-        add_card.setOnClickListener {
-            dialogview()
-        }
-
-     }
-
-    /*
-     * Method Runs when + Fab clicked
-     */
-    fun dialogview(){
-
-         print("Opening Dialog")
-
-
-     }
-
 }
--- app/src/main/java/me/parthahuja/studymate/subjects.kt
+++ app/src/main/java/me/parthahuja/studymate/subjects.kt
@@ -2,11 +2,50 @@ package me.parthahuja.studymate
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.v7.app.AlertDialog
+import android.widget.Button
+import android.widget.TextView
 
 class subjects : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_subjects)
+
+        //Add card Fab Id
+        var add_card = findViewById<FloatingActionButton>(R.id.add_button)
+
+        //Listener for Fab
+        add_card.setOnClickListener {
+            dialogview()
+        }
+
+    }
+
+    /*
+     * Method Runs when + Fab clicked to open inflated dialog
+     */
+    fun dialogview(){
+
+        print("Opening Dialog")
+
+        //importing and inflating custom layout add_dialog
+        val dialog = layoutInflater.inflate(R.layout.add_dialog, null)
+
+        //Accessing attributes inside dialog_layout
+        var textView = dialog.findViewById<TextView>(R.id.textView3)
+        var cancel = dialog.findViewById<Button>(R.id.button_cancel)
+
+        //Using Alert Builder with custom layout (dialog) inflated
+        val builder = AlertDialog.Builder(this)
+        builder.setView(dialog)
+        val window = builder.create()
+        window.show()
+
+        //cancel button listener
+        cancel.setOnClickListener {
+            window.dismiss()
+        }
     }
 }
